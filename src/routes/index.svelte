1bfcad6d2bc78f172cf8268fcec51aff7f6cfd3e
lmtr0 <lmtr0@protonmail.ch>
2022-06-10 11:12:13 -0300

    Connection worked :)

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -16,31 +16,8 @@
         console.log(provider)
         await provider.connect()
         console.log('Done')
-
-        const tx = {
-            from: provider.accounts[0], // Required
-            to: "0x5725B315107417C73fc24D77af53Bc1544495050", // Required (for non contract deployments)
-            data: "0x", // Required
-            gasPrice: "0x02540be400", // Optional
-            gas: "0x9c40", // Optional
-            value: "0x00", // Optional
-            nonce: "0x0114", // Optional
-        };
-
-        // await provider.createSession()
         await provider.killSession()
 
-        // Sign transaction
-        provider
-            .signTransaction(tx)
-            .then((result) => {
-                // Returns signed transaction
-                console.log(result);
-            })
-            .catch((error) => {
-                // Error returned when rejected
-                console.error(error);
-            });
     };
 
 	if (browser) main();
