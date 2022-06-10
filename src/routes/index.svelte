ef388f917c5f10bce7b89c503a97e4aa2e24462b
lmtr0 <lmtr0@protonmail.ch>
2022-06-10 09:30:43 -0300

    Working....

---
+++ src/routes/index.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { browser } from '$app/env';
+    import WallectConnect from "@walletconnect/browser"
+    import QrModal from "@walletconnect/qrcode-modal"
+
+	const main = async () => {
+        console.log('starting')
+        const provider = new WallectConnect({
+            bridge: "https://bridge.walletconnect.org", // Required
+            qrcodeModal: QrModal,
+        })
+        console.log('middle')
+        
+        console.log(provider)
+        await provider.connect()
+        console.log('Done')
+    };
+
+	if (browser) main();
+</script>
